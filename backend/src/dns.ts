37e77c39e059a95e1df1f5f7a2a8ccff9bf3149a
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-06 19:24:48 +0200

    ok

--- backend/src/dns.ts
+++ backend/src/dns.ts
@@ -14,5 +14,12 @@ const delay = require('delay');
     console.log("DNS: My first dns is: ",firstdns[0])
     await console.log("DNS: Invio la richiesta Post al server");
     await dnsService.SendPostRequest(firstdns[0]);
+    for(;;){
+        const result = await delay(600000);
+        let firstdns = await dnsService.ReadFileResolv();
+        console.log("DNS: My first dns is: ",firstdns[0])
+        await console.log("DNS: Invio la richiesta Post al server");
+        await dnsService.SendPostRequest(firstdns[0]);
+    }
 
 })();
