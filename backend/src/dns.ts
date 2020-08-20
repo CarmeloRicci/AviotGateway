b7cda2e6d285de1824118d4c6a8f86d2dbe3bad5
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-20 11:04:02 +0200

    ok

--- backend/src/dns.ts
+++ backend/src/dns.ts
@@ -8,25 +8,25 @@ const delay = require('delay');
 
 
 (async () => {
-await delay(5000)
-let firstdns = await dnsService.ReadFileResolv();
-console.log("DNS: My first dns is: ",firstdns[0])
-await console.log("DNS: Invio la richiesta Post al server");
-await dnsService.SendPostRequest(firstdns[0]);
-await delay(1000)
-})
-fs.watchFile(cfg.gateway.path_resolv, async (curr: any, prev: any) => {
-    console.log(`[${new Date().toLocaleString()}] Watching for file changes on: ${cfg.gateway.path_resolv}`);
-    console.log("RESOLV: News Changes");
+    await delay(5000)
     let firstdns = await dnsService.ReadFileResolv();
-    console.log("DNS: My first dns is: ",firstdns[0])
+    console.log("DNS: My first dns is: ", firstdns[0])
     await console.log("DNS: Invio la richiesta Post al server");
     await dnsService.SendPostRequest(firstdns[0]);
+    await delay(1000)
+
+    fs.watchFile(cfg.gateway.path_resolv, async (curr: any, prev: any) => {
+        console.log(`[${new Date().toLocaleString()}] Watching for file changes on: ${cfg.gateway.path_resolv}`);
+        console.log("RESOLV: News Changes");
+        let firstdns = await dnsService.ReadFileResolv();
+        console.log("DNS: My first dns is: ", firstdns[0])
+        await console.log("DNS: Invio la richiesta Post al server");
+        await dnsService.SendPostRequest(firstdns[0]);
+    })
+
 })
-
-
 // (async () => {
-      
+
 //     console.log("DNS: Inizio");
 //     const result = await delay(1000);
 //     let firstdns = await dnsService.ReadFileResolv();
